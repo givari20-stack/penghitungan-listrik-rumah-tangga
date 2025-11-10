aa8652e50379cb0c5d93184a08e6e9d6fe4e58e5
givari20-stack <givari20@gmail.com>
2025-11-10 12:06:05 +0700

    Add configuration settings for WiFi and sensors

---
+++ config.py
@@ -0,0 +1,13 @@
+# Konfigurasi WiFi
+SSID = "Your_WiFi_SSID"
+PASSWORD = "Your_WiFi_Password"
+
+# Konfigurasi Server
+SERVER_URL = "http://192.168.1.100:8000/api/esp32-data"
+DEVICE_ID = "ESP32_SmartHome_001"
+LOCATION = "Ruang_Tamu"
+
+# Kalibrasi Sensor
+LDR_DARK_THRESHOLD = 50      # Nilai LDR untuk kondisi gelap
+TEMP_HOT_THRESHOLD = 30      # Suhu untuk menyalakan kipas
+VOLTAGE_LOW_THRESHOLD = 200  # Tegangan rendah untuk alarm
